78fa9583eb897ca664f45548655bb08fd93d46c2
Helen Kyryliuk <helen@Helens-MacBook-Pro.local>
2019-02-05 18:51:18 +1300

    build finally created

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
     ],
   },
@@ -23,7 +18,4 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
-  devServer: {
-    contentBase: './dist',
-  },
 };
