99c81406d4b03ecc32bf73a973252db9ff7acf5b
Helen Kyryliuk <helen@Helens-MacBook-Pro.local>
2019-02-25 15:54:40 +1300

    disabled reset for input for error while loading channel, include from lodash replaced filter for validation, states updated

--- src/application.js
+++ src/application.js
@@ -5,31 +5,32 @@ import {
 } from 'melanke-watchjs';
 import parseXML from './XMLparser';
 import renderChannel from './renderers';
+import _ from 'lodash';
 
 export default () => {
   const state = {
-    isValidLink: null,
+    linkValidationState: null,
     channels: [],
     channelLinks: [],
-    isChannelLoaded: null,
+    channelLoadingState: null,
   };
 
-  watch(state, 'isValidLink', () => {
+  watch(state, 'linkValidationState', () => {
     const rssLinkInputBorder = document.getElementById('rssLinkInput');
     const validationMessage = document.getElementById('feedback');
-    if (state.isValidLink === 'notvalid') {
+    if (state.linkValidationState === 'notvalid') {
       rssLinkInputBorder.classList.remove('is-valid');
       rssLinkInputBorder.classList.add('is-invalid');
       validationMessage.classList.remove('valid-feedback');
       validationMessage.classList.add('invalid-feedback');
       validationMessage.textContent = 'Please provide a valid RSS feed link.';
-    } else if (state.isValidLink === 'valid') {
+    } else if (state.linkValidationState === 'valid') {
       rssLinkInputBorder.classList.remove('is-invalid');
       rssLinkInputBorder.classList.add('is-valid');
       validationMessage.classList.remove('invalid-feedback');
       validationMessage.classList.add('valid-feedback');
       validationMessage.textContent = 'Success!';
-    } else if (state.isValidLink === 'linkAlreadyExists') {
+    } else if (state.linkValidationState === 'linkAlreadyExists') {
       rssLinkInputBorder.classList.remove('is-valid');
       rssLinkInputBorder.classList.add('is-invalid');
       validationMessage.classList.remove('valid-feedback');
@@ -44,12 +45,12 @@ export default () => {
     }
   });
 
-  watch(state, 'isChannelLoaded', () => {
+  watch(state, 'channelLoadingState', () => {
     const rssLinkInputBorder = document.getElementById('rssLinkInput');
     const validationMessage = document.getElementById('feedback');
     const submitButton = document.getElementById('buttonOnSubmit');
     const alert = document.querySelector('div.alert');
-    if (state.isChannelLoaded === 'inProgress') {
+    if (state.channelLoadingState === 'inProgress') {
       alert.classList.remove('alert-warning');
       alert.classList.remove('alert-success');
       alert.classList.add('alert-info');
@@ -58,7 +59,7 @@ export default () => {
       submitButton.classList.add('disabled');
       rssLinkInputBorder.setAttribute('readonly', true);
       validationMessage.textContent = '';
-    } else if (state.isChannelLoaded === 'succeeded') {
+    } else if (state.channelLoadingState === 'succeeded') {
       alert.classList.remove('alert-info');
       alert.classList.remove('alert-warning');
       alert.classList.add('alert-success');
@@ -69,12 +70,11 @@ export default () => {
       rssLinkInputBorder.classList.remove('is-valid');
       validationMessage.classList.remove('valid-feedback');
       validationMessage.textContent = '';
-    } else if (state.isChannelLoaded === 'failed') {
+    } else if (state.channelLoadingState === 'failed') {
       alert.classList.remove('alert-info');
       alert.classList.add('alert-warning');
       alert.textContent = 'Failed to load the channel. Please try again.';
       submitButton.classList.remove('disabled');
-      rssLinkInputBorder.value = '';
       rssLinkInputBorder.removeAttribute('readonly');
       rssLinkInputBorder.classList.remove('is-valid');
       validationMessage.classList.remove('valid-feedback');
@@ -99,16 +99,16 @@ export default () => {
 
   const inputField = document.getElementById('rssLinkInput');
   inputField.addEventListener('input', (e) => {
-    state.isChannelLoaded = null;
-    const checkIfChannelExists = state.channelLinks.filter(link => link === e.target.value);
-    if (validator.isURL(e.target.value) && (checkIfChannelExists.length === 0)) {
-      state.isValidLink = 'valid';
-    } else if (checkIfChannelExists.length > 0) {
-      state.isValidLink = 'linkAlreadyExists';
+    state.channelLoadingState = null;
+    const checkIfChannelExists = _.includes(state.channelLinks, e.target.value);
+    if (validator.isURL(e.target.value) && (checkIfChannelExists === false)) {
+      state.linkValidationState = 'valid';
+    } else if (checkIfChannelExists === true) {
+      state.linkValidationState = 'linkAlreadyExists';
     } else if (e.target.value === '') {
-      state.isValidLink = null;
+      state.linkValidationState = null;
     } else {
-      state.isValidLink = 'notvalid';
+      state.linkValidationState = 'notvalid';
     }
   });
 
@@ -116,22 +116,22 @@ export default () => {
   element.addEventListener('click', () => {
     const channelLink = document.getElementById('rssLinkInput').value;
     if ((channelLink === '')) {
-      state.isValidLink = 'notvalid';
+      state.linkValidationState = 'notvalid';
       return;
     }
-    if (state.isValidLink === 'linkAlreadyExists') {
+    if (state.linkValidationState === 'linkAlreadyExists') {
       return;
     }
-    state.isChannelLoaded = 'inProgress';
+    state.channelLoadingState = 'inProgress';
     const corsApiUrl = 'https://cors-anywhere.herokuapp.com/';
     axios.get(`${corsApiUrl}${channelLink}`).then((res) => {
-      state.isValidLink = null;
+      state.linkValidationState = null;
       state.channels = [parseXML(res.data), ...state.channels];
       state.channelLinks = [channelLink, ...state.channelLinks];
-      state.isChannelLoaded = 'succeeded';
+      state.channelLoadingState = 'succeeded';
     }).catch(() => {
-      state.isChannelLoaded = 'failed';
-      state.isValidLink = null;
+      state.channelLoadingState = 'failed';
+      state.linkValidationState = null;
     });
   });
 };
