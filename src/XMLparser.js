3f684ac9bd04b3775a542050889c092269613126
Helen Kyryliuk <hkyryliuk@th2.com>
2019-09-08 15:24:28 +1200

    comments removed

--- src/XMLparser.js
+++ src/XMLparser.js
@@ -3,14 +3,12 @@ const parseXML = (data) => {
   const doc = domParser.parseFromString(data, 'text/xml');
 
   const items = doc.querySelectorAll('item');
-  const parseArticles = [...items].map(item => {
-    return ({
-      link: item.querySelector('link').textContent,
-      title: item.querySelector('title').textContent,
-      description: item.querySelector('description').textContent,
-      channelMedia: item.querySelector("thumbnail") && item.querySelector("thumbnail").textContent,
-    })
-  } );
+  const parseArticles = [...items].map(item => ({
+    link: item.querySelector('link').textContent,
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    channelMedia: item.querySelector('thumbnail') && item.querySelector('thumbnail').textContent,
+  }));
 
   return ({
     channelTitle: doc.querySelector('title').textContent,
